d62ced5770a61998d0063ca290e2a8f175a4b0c7
Morgan <mbayer.school@gmail.com>
2015-07-19 17:50:19 -0700

    Added pre and post incrementing

--- Apples.java
+++ Apples.java
@@ -5,8 +5,10 @@ class Apples {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); //change
 		int tuna = 5;
+		int tuna2 = 5;
 		int bass = 18;
-		System.out.println(++tuna);
+		System.out.println(++tuna); //Preincrementing
+		System.out.println(tuna2++); //Postincrementing
 	}
 
 }
