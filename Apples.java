3c730063e04124b3bdba3214ea224436c3eb6127
Morgan <mbayer.school@gmail.com>
2015-07-19 17:47:01 -0700

    First run

--- Apples.java
+++ Apples.java
@@ -5,7 +5,9 @@ class Apples {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); //change
 		int tuna = 5;
-
+		int bass = 18;
+		++tuna;
+		System.out.println(tuna);
 	}
 
 }
